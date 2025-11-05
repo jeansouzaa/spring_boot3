5fad4d822a5f6c04051c81cccb6f9620e41a0e9f
Jean Vitor de Souza <jean.souza@grupohcm.com.br>
2025-11-05 12:48:58 -0300

    Criado arquivo de configuração de CORS, criado DTO para médico e endereço, e criado controller do medico, que recenerá os dados do médico enviados via requisição

---
+++ src/main/java/med/voll/api/medico/DadosCadastradoMedico.java
@@ -0,0 +1,7 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastradoMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+
+}
