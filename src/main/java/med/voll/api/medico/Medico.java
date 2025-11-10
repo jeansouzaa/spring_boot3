b7400afdc2f4ece5bf12d01ed330cea4e7b902e9
Jean Souza <jean.vsouza@gmail.com>
2025-11-10 19:46:49 -0300

    Commitando as alterações de configuração de db, repositório médico, e alterações no controller médico

---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,46 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import med.voll.api.endereco.Endereco;
+
+@Table(name= "medicos")
+@Entity(name="Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+@Setter
+public class Medico {
+	
+	public Medico(DadosCadastradoMedico dados) {
+		this.nome = dados.nome();
+		this.email = dados.email();
+		this.crm = dados.crm();
+		this.endereco = new Endereco(dados.endereco());
+		this.especialidade = dados.especialidade();
+	}
+
+	@Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String email;
+	private String crm;
+	
+	@Enumerated(EnumType.STRING)
+	private Especialidade especialidade;
+	
+	@Embedded
+	private Endereco endereco;
+}
